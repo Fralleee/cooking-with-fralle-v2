ad02db9861961602fb4a8d9ae05ceebcdf3b1189
Roland Chelwing <jolleee@hotmail.com>
2023-09-20 15:30:03 +0200

    some fiddling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,11 +4,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-sky-300">
       <header className="relative capitalize font-bold w-full text-center py-4 lg:py-8 bg-header">
-        <div className="relative max-w-fit mx-auto bg-stone-100 rounded-full">
+        <div className="relative max-w-fit mx-auto bg-stone-100 rounded-full animate-title">
           <Image src={"/logo.webp"} alt={"Cooking with Fralle"} width={320} height={320} priority className="w-full h-auto" />
         </div>
       </header>
-      <main className="flex flex-col items-center justify-between bg-stone-100">
+      <main className="relative z-10 flex flex-col items-center justify-between bg-stone-100">
         {children}
       </main>
     </div>
