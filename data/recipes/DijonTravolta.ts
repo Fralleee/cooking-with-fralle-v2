ea840fe130120ed3f2c1f10ed3d9920b5b480e33
Roland Chelwing <jolleee@hotmail.com>
2023-09-21 13:52:38 +0200

    Update dijon image

--- data/recipes/DijonTravolta.ts
+++ data/recipes/DijonTravolta.ts
@@ -2,31 +2,31 @@ import { drinkKeywords } from "@/data/keywords";
 import { Recipe } from "@/types/recipe";
 
 const DijonTravolta: Recipe = {
-  title: "Dijon Travolta",
-  slug: "dijon-travolta",
-  color: "orange",
-  image: "cocktail",
-  keywords: ["dijon", "travolta", "tequila", "citron", ...drinkKeywords],
-  defaultServings: 2,
-  ingredients: [
-    {
-      label: "",
-      data: [
-        { label: "Tequila", amount: 160, measurement: "drinkvolume" },
-        { label: "Cointreau", amount: 80, measurement: "drinkvolume" },
-        { label: "Agavesirap", amount: 40, measurement: "drinkvolume" },
-        { label: "Tonic", amount: 400, measurement: "drinkvolume" },
-        { label: "Citroner", amount: 2, measurement: "pieces" },
-        { label: "Is" },
-      ],
-    },
-  ],
-  instructions: [
-    "Blanda alla ingredienser utom tonic och is i en shaker.",
-    "Skaka väl ihop med en stor isbit.",
-    "Fyll drinkglas med is och häll i blandningen.",
-    "Toppa med tonic.",
-  ],
+    title: "Dijon Travolta",
+    slug: "dijon-travolta",
+    color: "orange",
+    image: "cocktail",
+    keywords: ["dijon", "travolta", "tequila", "citron", ...drinkKeywords],
+    defaultServings: 2,
+    ingredients: [
+        {
+            label: "",
+            data: [
+                { label: "Tequila", amount: 160, measurement: "drinkvolume" },
+                { label: "Cointreau", amount: 80, measurement: "drinkvolume" },
+                { label: "Agavesirap", amount: 40, measurement: "drinkvolume" },
+                { label: "Tonic", amount: 400, measurement: "drinkvolume" },
+                { label: "Citroner", amount: 2, measurement: "pieces" },
+                { label: "Is" },
+            ],
+        },
+    ],
+    instructions: [
+        "Blanda alla ingredienser utom tonic och is i en shaker.",
+        "Skaka väl ihop med en stor isbit.",
+        "Fyll drinkglas med is och häll i blandningen.",
+        "Toppa med tonic.",
+    ],
 };
 
 export default DijonTravolta;
