b2cc4cc8250ca5371cfe442a73572c096aed3756
Roland Chelwing <jolleee@hotmail.com>
2023-09-24 20:33:59 +0200

    Add parallax effect to header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,10 @@
+import Header from "@/components/header";
 import Image from "next/image";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="bg-sky-300">
-            <header className="relative grid min-h-0 w-full place-items-center bg-header py-4 text-center font-bold capitalize sm:min-h-[440px] lg:py-8">
-                <div className="relative max-w-fit scale-0 animate-title rounded-full bg-stone-100">
-                    <Image
-                        src={"/logo.webp"}
-                        alt={"Cooking with Fralle"}
-                        width={320}
-                        height={320}
-                        priority
-                        className="h-auto w-full"
-                    />
-                </div>
-            </header>
+            <Header />
             <main className="relative z-10 flex flex-col items-center justify-between bg-stone-100">
                 {children}
             </main>
