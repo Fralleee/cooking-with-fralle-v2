3e9794abf1ce4cf6879a00a99144c73187ba1448
Roland Chelwing <jolleee@hotmail.com>
2023-09-28 10:19:13 +0200

    Fix transition performance

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -11,9 +11,9 @@ const Child = forwardRef<
     return (
         <motion.div
             ref={ref}
-            initial={{ opacity: 0, y: 50, scale: 0.95 }}
-            animate={{ opacity: 1, y: 0, scale: 1 }}
-            exit={{ opacity: 0, y: 50, scale: 1.05 }}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
             transition={{ duration: 0.1 }}
             onAnimationComplete={() => window.scrollTo(0, 0)}
         >
