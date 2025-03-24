94ff3a44e5d6b9043f11ced7b94efd49c3c2b1e9
Roland Chelwing <jolleee@hotmail.com>
2025-03-24 10:31:06 +0100

    chore: try removing scroll

--- app/(root)/(components)/header.tsx
+++ app/(root)/(components)/header.tsx
@@ -7,27 +7,27 @@ export default function Header() {
 	const [scrollY, setScrollY] = useState(0);
 	const [maxScroll, setMaxScroll] = useState(400);
 
-	useLayoutEffect(() => {
-		const handleScroll = () => {
-			setScrollY(window.scrollY);
-		};
+	// useLayoutEffect(() => {
+	// 	const handleScroll = () => {
+	// 		setScrollY(window.scrollY);
+	// 	};
 
-		const handleResize = () => {
-			if (headerRef.current) {
-				setMaxScroll(Math.max(366, headerRef.current.clientHeight));
-			}
-		};
+	// 	const handleResize = () => {
+	// 		if (headerRef.current) {
+	// 			setMaxScroll(Math.max(366, headerRef.current.clientHeight));
+	// 		}
+	// 	};
 
-		window.addEventListener("scroll", handleScroll);
-		window.addEventListener("resize", handleResize);
+	// 	window.addEventListener("scroll", handleScroll);
+	// 	window.addEventListener("resize", handleResize);
 
-		handleResize();
+	// 	handleResize();
 
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
+	// 	return () => {
+	// 		window.removeEventListener("scroll", handleScroll);
+	// 		window.removeEventListener("resize", handleResize);
+	// 	};
+	// }, []);
 
 	const scale = Math.max(0.6, 1 - (scrollY / maxScroll) * 0.25);
 	const opacity = Math.max(0.5, 1 - (scrollY / maxScroll) * 0.5);
@@ -38,9 +38,9 @@ export default function Header() {
 			ref={headerRef}
 			className="relative grid min-h-0 w-full place-items-center bg-header py-4 text-center font-bold capitalize sm:min-h-[440px] lg:py-8"
 		>
-			<div className="relative max-w-fit animate-title">
+			<div className="animate-title">
 				<div
-					className="relative max-w-fit rounded-full bg-stone-100"
+					className="rounded-full bg-stone-100"
 					style={{
 						transform: `translateY(${translateY}px) scale(${scale})`,
 						scale,
