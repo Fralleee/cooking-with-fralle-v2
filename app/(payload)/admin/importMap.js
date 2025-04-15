685588618993fe6b76b3a8cc6fff968492db4193
Roland Chelwing <jolleee@hotmail.com>
2025-04-15 09:41:25 +0200

    chore: fix vercel blob

--- app/(payload)/admin/importMap.js
+++ app/(payload)/admin/importMap.js
@@ -1,5 +1,7 @@
 import { default as default_e279831ce798b6a62db4ee5a823cf2d4 } from '../../../app/(payload)/_components/color-picker'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
-  "./app/(payload)/_components/color-picker#default": default_e279831ce798b6a62db4ee5a823cf2d4
+  "./app/(payload)/_components/color-picker#default": default_e279831ce798b6a62db4ee5a823cf2d4,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e
 }
